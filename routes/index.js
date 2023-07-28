4e1db2019fa3ebefe19dd5c5255a235e0b3fdb94
lhgfrontis <85724878+AmaneKanata@users.noreply.github.com>
2023-07-28 15:31:07 +0900

    pod 가 발급받은 토큰을 사용하도록 수정

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ const axios = require("axios");
 const fs = require("fs");
 
 const kubeAddress = "https://agones-dns-8r8rb6bk.hcp.koreacentral.azmk8s.io";
-const token = "wck52cmcm2sn7iqed9cv2irxdql3bzusbahzbwpeg1nqiwhb9tingqva0egm53ojd2jxrs8o14rjcxaeii0od7w16kd0qvvr8gyo0tw6zs09sat80356n9zdsxzotcr9";
+//const token = "wck52cmcm2sn7iqed9cv2irxdql3bzusbahzbwpeg1nqiwhb9tingqva0egm53ojd2jxrs8o14rjcxaeii0od7w16kd0qvvr8gyo0tw6zs09sat80356n9zdsxzotcr9";
+const token = fs.readFileSync(
+  "/var/run/secrets/kubernetes.io/serviceaccount/token",
+  "utf8"
+);
 const namespace = "default";
 
 const config = {
@@ -13,9 +17,9 @@ const config = {
     "Authorization": `Bearer ${token}`,
     "Content-Type": "application/json",
   },
-  httpsAgent: new (require('https').Agent)({
-    rejectUnauthorized: false
-  })
+  // httpsAgent: new (require('https').Agent)({
+  //   rejectUnauthorized: false
+  // })
 };
 
 const gameServerAllocationAddress = `${kubeAddress}/apis/allocation.agones.dev/v1/namespaces/${namespace}/gameserverallocations`
